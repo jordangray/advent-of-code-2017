05db669274c9cf181193b84688c2627f7526feb7
Jordan Gray <j.b.gray@gmail.com>
2017-12-11 01:30:45 +0000

    Solution for day 1, part 2
    Just noticed there's a part 2!

---
+++ day-1/sum-halfway.js
@@ -0,0 +1,10 @@
+const halfwayAround = (str, from) => {
+  const length = str.length
+  const index = (from + length / 2) % length
+  return str[index]
+}
+
+const sumHalfway = input => [...input]
+  .filter((char, index) => char === halfwayAround(input, index))
+  .map(str => +str)
+  .reduce((a, b) => a + b, 0)
