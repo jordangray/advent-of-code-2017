448944895816b7eaccc4149748a87537af2436d3
Jordan Gray <j.b.gray@gmail.com>
2017-12-11 18:56:44 +0000

    Solution for day 2 part 2

---
+++ day-2/quotientSum.js
@@ -0,0 +1,19 @@
+const rows = sheet => sheet.split('\n')
+const cells = row => row.split('\t').map(cell => +cell)
+
+const divisorOf = n => m => n % m === 0
+
+const quotient = nums => nums
+  .sort((a, b) => b - a)
+  .reduce((result, next, i) =>
+    !isNaN(result)
+      ? result
+      : next / nums.slice(i + 1).find(divisorOf(next))
+  , NaN)
+
+const quotientSum = input => rows(input)
+  .map(row => cells(row))
+  .map(quotient)
+  .reduce((a, b) => a + b, 0)
+
+export default quotientSum
